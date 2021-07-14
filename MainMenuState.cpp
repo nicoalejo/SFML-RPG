2912fd02456301e642866814ed4dec5e8c3c902e
Nicolas Rojas <nicoalejor@gmail.com>
2021-07-13 19:39:57 -0500

    Video 18. Background implemented

--- MainMenuState.cpp
+++ MainMenuState.cpp
@@ -1,5 +1,19 @@
 #include "MainMenuState.h"
 
+//Initializar Functions
+void MainMenuState::initVariables()
+{
+}
+
+void MainMenuState::initBackground()
+{
+	this->background.setSize(sf::Vector2f(static_cast<float>(this->window->getSize().x), static_cast<float>(this->window->getSize().y)));
+	if (!this->backgroundTexture.loadFromFile("Resources/Images/Backgrounds/bg1.jpg")) {
+		throw "ERROR::MAIN MENU STATE::FAILED TO LOAD BACKGROUND TEXTURE";
+	}
+	this->background.setTexture(&this->backgroundTexture);
+}
+
 void MainMenuState::initFonts()
 {
 	if (!this->font.loadFromFile("Fonts/Enchanted-Land.otf")) {
@@ -24,11 +38,15 @@ void MainMenuState::initKeybinds()
 
 void MainMenuState::initButtons()
 {
-	this->buttons["GAME_STATE"] = new Button(100, 100, 300, 100,
+	this->buttons["GAME_STATE"] = new Button(150, 100, 300, 100,
 		&this->font, "New Game",
 		sf::Color(70,70,70,200), sf::Color(150, 150, 150, 255), sf::Color(20, 20, 20, 200));
 
-	this->buttons["EXIT_STATE"] = new Button(100, 300, 300, 100,
+	this->buttons["SETTINGS"] = new Button(150, 300, 300, 100,
+		&this->font, "Settings",
+		sf::Color(70, 70, 70, 200), sf::Color(150, 150, 150, 255), sf::Color(20, 20, 20, 200));
+
+	this->buttons["EXIT_STATE"] = new Button(150, 500, 300, 100,
 		&this->font, "Quit",
 		sf::Color(70, 70, 70, 200), sf::Color(150, 150, 150, 255), sf::Color(20, 20, 20, 200));
 }
@@ -36,12 +54,11 @@ void MainMenuState::initButtons()
 MainMenuState::MainMenuState(sf::RenderWindow* window, std::map<std::string, int>* supportedKeys, std::stack<State*>* states)
 	:State(window, supportedKeys, states)
 {
+	this->initVariables();
+	this->initBackground();
 	this->initFonts();
 	this->initKeybinds();
 	this->initButtons();	
-
-	this->background.setSize(sf::Vector2f(window->getSize().x, window->getSize().y));
-	this->background.setFillColor(sf::Color::Magenta);
 }
 
 MainMenuState::~MainMenuState()
@@ -90,8 +107,7 @@ void MainMenuState::Update(const float& dt)
 	this->updateInput(dt);
 
 	this->updateButtons();	
-}
-	
+}	
 
 void MainMenuState::renderButtons(sf::RenderTarget* target)
 {
@@ -109,5 +125,16 @@ void MainMenuState::Render(sf::RenderTarget* target)
 	target->draw(this->background);	
 
 	this->renderButtons(target);
+
+	//REMOVE LATER
+	/*sf::Text mouseText;
+	mouseText.setPosition(this->mousePosView.x, this->mousePosView.y -50);
+	mouseText.setFont(this->font);
+	mouseText.setCharacterSize(36);
+	std::stringstream ss;
+	ss << this->mousePosView.x << " " << this->mousePosView.y;
+	mouseText.setString(ss.str());
+
+	target->draw(mouseText);*/
 }
 
